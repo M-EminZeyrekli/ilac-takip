24a4e06f7c66922171e6aa16ae01f244452e4b57
Muhammet Emin ZEYREKLİ <20120205050@ismu.edu.tr>
2022-10-29 21:09:56 +0300

    project has been implemented

---
+++ api/src/main/java/com/medicinetracking/api/controllers/MedicineController.java
@@ -0,0 +1,59 @@
+package com.medicinetracking.api.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medicinetracking.api.entities.Medicine;
+import com.medicinetracking.api.services.Abstract.MedicineService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/medicine")
+@CrossOrigin
+@RequiredArgsConstructor
+public class MedicineController {
+    private final MedicineService medicineService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllMedicine() {
+        List<Medicine> list = medicineService.getAllMedicine();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneMedicine(@PathVariable(name = "id", required = true) int id) {
+        Medicine medicine = medicineService.getOneMedicine(id);
+        return new ResponseEntity<>(medicine, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> postOneMedicine(@RequestBody Medicine medicine) {
+        Medicine medicineAdded = medicineService.postOneMedicine(medicine);
+        return new ResponseEntity<>(medicineAdded, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putOneMedicine(@PathVariable(name = "id", required = true) int id,
+            @RequestBody Medicine medicine) {
+        Medicine medicineUpdated = medicineService.putOneMedicine(id, medicine);
+        return new ResponseEntity<>(medicineUpdated, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOneMedicine(@PathVariable int id) {
+        medicineService.deleteOneMedicine(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ api/src/main/java/com/medicinetracking/api/controllers/PatientController.java
@@ -0,0 +1,60 @@
+package com.medicinetracking.api.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medicinetracking.api.entities.Patient;
+import com.medicinetracking.api.services.Abstract.PatientService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/patient")
+@CrossOrigin
+@RequiredArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllPatient() {
+        List<Patient> list = patientService.getAllPatient();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOnePatient(@PathVariable(name = "id", required = true) int id) {
+        Patient patient = patientService.getOnePatient(id);
+        return new ResponseEntity<>(patient, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> postOnePatient(@RequestBody Patient patient) {
+        Patient patientAdded = patientService.postOnePatient(patient);
+        return new ResponseEntity<>(patientAdded, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putOnePatient(@PathVariable(name = "id", required = true) int id,
+            @RequestBody Patient patient) {
+        Patient patientUpdated = patientService.putOnePatient(id, patient);
+        return new ResponseEntity<>(patientUpdated, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOnePatient(@PathVariable int id) {
+        patientService.deleteOnePatient(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}

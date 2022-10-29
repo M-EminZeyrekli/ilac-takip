bf622ff3f1fed417dd4c19fc190a86fdbce520c1
Muhammet Emin ZEYREKLİ <20120205050@ismu.edu.tr>
2022-10-29 16:58:42 +0300

    entities has been implemented

--- api/src/main/java/com/medicinetracking/api/entities/Medicine.java
+++ api/src/main/java/com/medicinetracking/api/entities/Medicine.java
@@ -1,5 +1,30 @@
 package com.medicinetracking.api.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "medicine")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Medicine {
     
+    @Id
+    @GeneratedValue
+    @Column(name = "medicine_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "medicine_type")
+    private String medicineType;
 }
--- api/src/main/java/com/medicinetracking/api/entities/Patient.java
+++ api/src/main/java/com/medicinetracking/api/entities/Patient.java
@@ -1,5 +1,47 @@
 package com.medicinetracking.api.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "patient")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Patient {
+    @Id
+    @GeneratedValue
+    @Column(name = "patient_id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "weight")
+    private int weight;
+
+    @Column(name = "height")
+    private double height;
+
+    @Column(name = "phone_number")
+    private int phoneNumber;
+    
+    @Column(name = "gender")
+    private String gender;
+
     
 }

94b99c2bca0b074988d9bbfa36904c60a3870251
Muhammet Emin ZEYREKLİ <20120205050@ismu.edu.tr>
2022-11-30 18:11:00 +0300

    MedicineList has been updated

--- frontend/medicine-tracking/src/components/medicine/MedicineList.js
+++ frontend/medicine-tracking/src/components/medicine/MedicineList.js
@@ -6,58 +6,59 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Checkbox } from "@mui/material";
-
+import { Button, Checkbox } from "@mui/material";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function MedicineList({ medicines }) {
-
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Monday</TableCell>
-            <TableCell>Tuesday</TableCell>
-            <TableCell>Wednesday</TableCell>
-            <TableCell>Thursday</TableCell>
-            <TableCell>Friday</TableCell>
-            <TableCell>Saturday</TableCell>
-            <TableCell>Sunday</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {medicines.map((medicine) => (
-            <TableRow
-              key={medicine.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {medicine.name} <Checkbox {...label} />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {medicine.name} <Checkbox {...label} />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {medicine.name} <Checkbox {...label} />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {medicine.name} <Checkbox {...label} />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {medicine.name} <Checkbox {...label} />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {medicine.name} <Checkbox {...label} />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {medicine.name} <Checkbox {...label} />
-              </TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Monday</TableCell>
+              <TableCell>Tuesday</TableCell>
+              <TableCell>Wednesday</TableCell>
+              <TableCell>Thursday</TableCell>
+              <TableCell>Friday</TableCell>
+              <TableCell>Saturday</TableCell>
+              <TableCell>Sunday</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {medicines.map((medicine) => (
+              <TableRow
+                key={medicine.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {medicine.name} <Checkbox {...label} />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {medicine.name} <Checkbox {...label} />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {medicine.name} <Checkbox {...label} />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {medicine.name} <Checkbox {...label} />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {medicine.name} <Checkbox {...label} />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {medicine.name} <Checkbox {...label} />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {medicine.name} <Checkbox {...label} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button>Update</Button>
+    </div>
   );
 }
